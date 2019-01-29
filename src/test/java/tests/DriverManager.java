5d68684db3a8459786b3869055ab7fabe2e5f327
Роман Мотовилов <rmotovilov@web100-zp.local>
2019-01-29 17:26:09 +0200

    Allure test for Ubuntu - solved problem with driver

--- src/test/java/tests/DriverManager.java
+++ src/test/java/tests/DriverManager.java
@@ -18,13 +18,6 @@ public class DriverManager {
             System.setProperty("webdriver.chrome.driver", path + "/drivers/chromedriver");
 
             ChromeOptions options = new ChromeOptions();
-//            options.addArguments("start-maximized"); // open Browser in maximized mode
-//            options.addArguments("disable-infobars"); // disabling infobars
-//            options.addArguments("--disable-extensions"); // disabling extensions
-//            options.addArguments("--disable-gpu"); // applicable to windows os only
-//            options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-//            options.addArguments("--no-sandbox"); // Bypass OS security model
-
             options.addArguments("--headless");
             options.addArguments("--disable-gpu");
             options.addArguments("window-size=1024,768");
