b5e582a376080947b08f57294d48fae135f4c30b
Роман Мотовилов <rmotovilov@web100-zp.local>
2019-01-29 16:28:18 +0200

    Allure test for Ubuntu 8

--- src/test/java/tests/DriverManager.java
+++ src/test/java/tests/DriverManager.java
@@ -18,12 +18,13 @@ public class DriverManager {
             System.setProperty("webdriver.chrome.driver", path + "/drivers/chromedriver");
 
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("start-maximized"); // open Browser in maximized mode
-            options.addArguments("disable-infobars"); // disabling infobars
-            options.addArguments("--disable-extensions"); // disabling extensions
-            options.addArguments("--disable-gpu"); // applicable to windows os only
-            options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-            options.addArguments("--no-sandbox"); // Bypass OS security model
+//            options.addArguments("start-maximized"); // open Browser in maximized mode
+//            options.addArguments("disable-infobars"); // disabling infobars
+//            options.addArguments("--disable-extensions"); // disabling extensions
+//            options.addArguments("--disable-gpu"); // applicable to windows os only
+//            options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//            options.addArguments("--no-sandbox"); // Bypass OS security model
+            options.setExperimentalOption("useAutomationExtension", false);
 
             webDriver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("firefox")) {
