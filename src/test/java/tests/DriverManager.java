bbcc9c0f05fd70d2df209e9f98a016bfb35356ec
Роман Мотовилов <rmotovilov@web100-zp.local>
2019-01-30 14:04:49 +0200

    Added OsCheck

--- src/test/java/tests/DriverManager.java
+++ src/test/java/tests/DriverManager.java
@@ -4,32 +4,49 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import java.util.concurrent.TimeUnit;
 
 public class DriverManager {
 
     private static WebDriver webDriver;
-    private static String path;
+    private static String path = System.getProperty("user.dir");
+
+
+    public static String getPath() {
+        return path;
+    }
+
+    public static void setPath(String path) {
+        DriverManager.path += path;
+    }
 
     public static WebDriver getDriverManager(String browser) {
-        path = System.getProperty("user.dir");
-        if (browser.equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.driver", path + "/drivers/chromedriver.exe");
+//        path = System.getProperty("user.dir");
 
+        OsCheck.OSType os = OsCheck.getOperatingSystemType();
+        if (os == OsCheck.OSType.LINUX && browser.equalsIgnoreCase("chrome")) {
+            setPath("/drivers/chromedriver");
+            System.setProperty("webdriver.chrome.driver", getPath());
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--headless");
             options.addArguments("--disable-gpu");
             options.addArguments("window-size=1024,768");
             options.addArguments("--no-sandbox");
             options.setExperimentalOption("useAutomationExtension", false);
-
             webDriver = new ChromeDriver(options);
-        } else if (browser.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.gecko.driver", path + "/drivers/geckodriver.exe");
+        } else if (os == OsCheck.OSType.WINDOWS && browser.equalsIgnoreCase("chrome")) {
+            setPath("/drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", getPath());
+            webDriver = new ChromeDriver();
+        } else if (os == OsCheck.OSType.WINDOWS && browser.equalsIgnoreCase("firefox")) {
+            setPath("/drivers/geckodriver.exe");
+            System.setProperty("webdriver.gecko.driver", getPath());
             webDriver = new FirefoxDriver();
-        } else if (browser.equalsIgnoreCase("ie")){
-            System.setProperty("webdriver.ie.driver", path + "/drivers/IEDriverServer.exe");
+        } else if (os == OsCheck.OSType.WINDOWS && browser.equalsIgnoreCase("ie")){
+            setPath("/drivers/IEDriverServer.exe");
+            System.setProperty("webdriver.ie.driver", getPath());
             webDriver = new InternetExplorerDriver();
         }
         setConfiguredDriver();
@@ -40,5 +57,9 @@ public class DriverManager {
         webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         webDriver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         webDriver.manage().window().maximize();
+    }
+
+    public static WebDriver getWebDriver() {
+        return webDriver;
     }
 }
---
+++ src/test/java/tests/OsCheck.java
@@ -0,0 +1,28 @@
+package tests;
+
+import java.util.Locale;
+
+public class OsCheck {
+
+    protected static OSType detectedOS;
+
+    public enum OSType {
+        WINDOWS, MAC_OS, LINUX, OTHER
+    };
+
+    public static OSType getOperatingSystemType() {
+        if (detectedOS == null) {
+            String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+            if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
+                detectedOS = OSType.MAC_OS;
+            } else if (OS.indexOf("win") >= 0) {
+                detectedOS = OSType.WINDOWS;
+            } else if (OS.indexOf("nux") >= 0) {
+                detectedOS = OSType.LINUX;
+            } else {
+                detectedOS = OSType.OTHER;
+            }
+        }
+        return detectedOS;
+    }
+}
